f337f20163d027661e3079e6809fb27cd6841bbe
Stefan Hermann <herma912@umn.edu>
2024-06-26 17:10:22 -0500

    first commit

---
+++ frontend/src/components/LoadingIndicator.jsx
@@ -0,0 +1,10 @@
+import "../styles/LoadingIndicator.css";
+
+const LoadingIdicator = () => {
+  return (
+    <div className="loading-container">
+      <div className="loader"></div>
+    </div>
+  );
+};
+export default LoadingIdicator;
